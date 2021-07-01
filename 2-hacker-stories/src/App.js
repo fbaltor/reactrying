b9834aed37a1f4abd194b2533d80f10f6acaceab
Felipe Baltor <fbaltor@gmail.com>
2021-07-01 14:51:52 -0300

    Just playing with App component in project 2

--- 2-hacker-stories/src/App.js
+++ 2-hacker-stories/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Simple React App</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <hr />
+      <div>
+        {list.map((el) => (
+          <div key={el.objectID}>
+            <span>{'Author: ' + el.author + ' '}</span>
+            <span>{'Comments: ' + el.num_comments + ' '}</span>
+            <span>{'Points ' + el.points}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
